6e202378b3eea24a93c9a4e092252f360b2ea1d0
Chad Loro <chad.loro.cl@gmail.com>
2019-10-08 11:22:33 -0700

    Added bar parser WIP

--- src/dsl/ast/Bar.ts
+++ src/dsl/ast/Bar.ts
@@ -1,8 +1,36 @@
 import { Node } from "../libs/Node";
+import Tokenizer from "../libs/Tokenizer";
+import Quarters from "./Quarters";
+import Rhythm from "./Rhythm";
+import Eighths from "./Eighths";
+import Sixteenths from "./Sixteenths";
 
 class Bar extends Node {
+  rhythm?: Rhythm;
+  rhythmName?: string;
+  
   parse(): void {
-    throw new Error("Method not implemented.");
+    let tokenizer = Tokenizer.getTokenizer();
+    console.log(tokenizer);
+
+    if (tokenizer.checkToken("|")) {
+      this.rhythm = new Quarters();
+      console.log("quarter")
+    }
+    else if (tokenizer.checkToken("[")) {
+      this.rhythm = new Eighths();
+      console.log("eighth")
+    }
+    else if (tokenizer.checkToken("{")) {
+      this.rhythm = new Sixteenths();
+      console.log("sixteenth")
+    } else {
+      this.rhythmName = tokenizer.getNext();
+    }
+
+    if (this.rhythm) {
+      this.rhythm.parse();
+    }
   }
   evaluate(): void {
     throw new Error("Method not implemented.");
--- src/dsl/ast/Repeat.ts
+++ src/dsl/ast/Repeat.ts
@@ -1,8 +1,18 @@
 import { Node } from "../libs/Node";
+import Tokenizer from "../libs/Tokenizer";
 
 class Repeat extends Node {
+  quantity: number;
+
   parse(): void {
-    throw new Error("Method not implemented.");
+    let tokenizer = Tokenizer.getTokenizer();
+    tokenizer.getAndCheckNext("(");
+    // make sure to check if rhythm lengths are consistent here
+    while()
+
+    tokenizer.getAndCheckNext(")");
+    tokenizer.getAndCheckNext("*");
+    this.quantity = parseInt(tokenizer.getNext())
   }
   evaluate(): void {
     throw new Error("Method not implemented.");
