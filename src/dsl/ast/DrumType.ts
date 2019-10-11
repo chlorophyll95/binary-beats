4fbd61b6c56cdb8cd6bd4ddeeaa2d0ba1cdbee43
Ben Zvikler <bzvikler@gmail.com>
2019-10-11 00:41:16 -0700

    Adds favicon lol

--- src/dsl/ast/DrumType.ts
+++ src/dsl/ast/DrumType.ts
@@ -5,6 +5,7 @@ export type DrumType =
   'TM2' |
   'TM3' |
   'HAT' |
+  'OHT' |
   'CSH' |
   'RDE' |
   'CLP';
--- src/dsl/libs/DrumCodeMap.ts
+++ src/dsl/libs/DrumCodeMap.ts
@@ -1,26 +1,30 @@
 import { DrumType } from '../ast/DrumType';
 
 class DrumCodeMap {
+  static drumCodes: number[] = [21, 71, 37, 55, 1, 80, 17, 79, 64, 32];
+
   static getDrumCode(drumName: DrumType): number {
     switch (drumName) {
       case 'CLP':
-        return 0;
+        return 21;
       case 'CSH':
-          return 0;
+        return 71;
       case 'HAT':
-        return 35;
+        return 37;
+      case 'OHT':
+        return 55;
       case 'KCK':
-        return 5;
+        return 1;
       case 'RDE':
-        return 0;
+        return 80;
       case 'SNR':
-        return 15;
+        return 17;
       case 'TM1':
-        return 0;
+        return 79;
       case 'TM2':
-        return 0;
+        return 64;
       case 'TM3':
-        return 0;
+        return 32;
       default:
         throw new Error('Invalid drum name');
     }
