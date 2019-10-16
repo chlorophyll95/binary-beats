17b2c626abbe304756d038f15776d3446efb78dd
Susan Chen <susanszc@gmail.com>
2019-10-15 22:02:27 -0700

    added some more drum types

--- src/dsl/ast/DrumType.ts
+++ src/dsl/ast/DrumType.ts
@@ -1,11 +1,17 @@
 export type DrumType =
-  'SNR' |
-  'KCK' |
-  'TM1' |
-  'TM2' |
-  'TM3' |
-  'HAT' |
-  'OHT' |
-  'CSH' |
-  'RDE' |
-  'CLP';
+  'SNR' | // snare
+  'KCK' | // kick
+  'TM1' | // tom 2
+  'TM2' | // tom 2
+  'TM3' | // tom 3
+  'HAT' | // hi hat
+  'OHT' | // open hi hat
+  'CSH' | // crash
+  'RDE' | // ride
+  'PED' | // pedal hi hat
+  'RBL' | // ride bell
+  'TMB' | // tambourine
+  'CAB' | // cabasa
+  'MAR' | // maraca
+  'MHC' | // mute high conga 
+  'CLP'; // clap
